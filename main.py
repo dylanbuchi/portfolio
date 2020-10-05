f6610fca47737d2537ccc1b6bb70c7ebbb95abd0
Dylan Buchi <devybuchi@gmail.com>
2020-10-05 20:20:52 -0300

    add templates images for website

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import flask
+from flask import Flask
+from flask.templating import render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_pages(page_name):
+    return render_template(page_name)
