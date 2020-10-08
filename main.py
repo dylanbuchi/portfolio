bcdaec56f25f5f44615dd21b7e33375a3b363a2a
Dylan Buchi <devybuchi@gmail.com>
2020-10-08 19:21:10 -0300

    add a submit form to contact

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from flask import Flask
+import csv
+
+from flask import Flask, request, url_for, redirect
 from flask.templating import render_template
+
 app = Flask(__name__)
+
+
+def write_to_csv_file(data: dict):
+    with open(r'data\data.csv', 'a', newline='') as csv_file:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+
+        csv_writer = csv.writer(csv_file,
+                                delimiter=',',
+                                quotechar='"',
+                                quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
 
 @app.route('/')
@@ -9,5 +25,16 @@ def home():
 
 
 @app.route('/<string:page_name>')
-def html_pages(page_name):
+def html_page(page_name):
     return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv_file(data)
+
+        return redirect('thank_you.html')
+    else:
+        return 'error'
