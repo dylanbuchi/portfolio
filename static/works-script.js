539c034038658cd86e98f0fbb540bdca4e9235b0
Dylan Buchi <devybuchi@gmail.com>
2020-10-31 05:20:03 -0300

    add procfile for heroku app

--- static/works-script.js
+++ static/works-script.js
@@ -4,18 +4,21 @@ $(".hjava").on("mouseover", function () {
     $(".hdart").removeClass("active-d");
     $(".hjs").removeClass("active-js");
 });
+
 $(".hpy").on("mouseover", function () {
     $(this).addClass("active-p");
     $(".hjava").removeClass("active-j");
     $(".hdart").removeClass("active-d");
     $(".hjs").removeClass("active-js");
 });
+
 $(".hdart").on("mouseover", function () {
     $(this).addClass("active-d");
     $(".hjava").removeClass("active-j");
     $(".hpy").removeClass("active-p");
     $(".hjs").removeClass("active-js");
 });
+
 $(".hjs").on("mouseover", function () {
     $(this).addClass("active-js");
     $(".hjava").removeClass("active-j");
